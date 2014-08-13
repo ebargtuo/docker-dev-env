5e81c5f90e5d80cf6ca702809276e0620a60b984
ebargtuo <ebargtuo@gmx.it>
2014-08-13 19:36:16 +0200

    Add check to make sure vagrant ansible set up provisioning step only runs once

--- provisioning/bootstrap-ansible-host.sh
+++ provisioning/bootstrap-ansible-host.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-apt-get -qq update
-apt-get -qq --yes install software-properties-common
-apt-add-repository --yes ppa:ansible/ansible
-apt-get -qq update
-apt-get -qq --yes install ansible
+[ -e /usr/bin/ansible ] || {
+  apt-get -qq update
+  apt-get -qq --yes install software-properties-common
+  apt-add-repository --yes ppa:ansible/ansible
+  apt-get -qq update
+  apt-get -qq --yes install ansible
+}
 
