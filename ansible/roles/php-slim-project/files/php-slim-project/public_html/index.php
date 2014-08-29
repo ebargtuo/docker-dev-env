e4779bae4eeeb87a8c1ecd34ebf945783ba117fd
ebargtuo <ebargtuo@gmx.it>
2014-08-29 19:09:57 +0000

    Expand slim project html index page

--- ansible/roles/php-slim-project/files/php-slim-project/public_html/index.php
+++ ansible/roles/php-slim-project/files/php-slim-project/public_html/index.php
@@ -1,2 +1,13 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Welcome to Slim</title>
+</head>
+<body>
+This is a PHP Slim framework example.
+</body>
+</html>
